0d9ffcdba26060901dd46524e12defc4eccbe4ce
shun <shun@getto.systems>
2016-10-26 15:39:42 +0000

    put attribute variables to container

--- lib/labelizer.rb
+++ lib/labelizer.rb
@@ -38,15 +38,18 @@ module Labelizer
           end
         end
 
-        model = self.to_s.underscore
         @labelized ||= Container.new([]){|attr_labelized,attr|
+          model = self.to_s.underscore
           attr_labelized[attr] = Container.new(__send__(attr.to_s.pluralize), accept_array_key: true){|value_labelized,value|
+            data = attr_labelized[attr].instance_variable_get(:@data)
+            attr_label_types = data[:label_types]
+            attr_converter = data[:converter]
             unless attr_labelized[attr].has_key?(value)
-              Container.new(label_types){|h,label_type|
+              Container.new(attr_label_types){|h,label_type|
                 h[label_type] = value.map{|val| value_labelized[val][label_type]}
               }
             else
-              value_labelized[value] = Container.new(label_types){|h,label_type|
+              value_labelized[value] = Container.new(attr_label_types){|h,label_type|
                 if label_type == :value
                   result = value
                 else
@@ -59,7 +62,7 @@ module Labelizer
                       "",
                     ],
                   )
-                  if c = converter[label_type]
+                  if c = attr_converter[label_type]
                     result = c[result]
                   end
                 end
@@ -77,6 +80,9 @@ module Labelizer
               }
             end
           end
+          data = attr_labelized[attr].instance_variable_get(:@data)
+          data[:label_types] = label_types
+          data[:converter] = converter
 
           attr_labelized[attr]
         }
